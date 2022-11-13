da953ec7951d0fe375b82c63fb99baad73c013ab
ksfgn <shapovalovalex938@gmail.com>
2022-11-13 21:19:49 +0200

    modal added

--- 1/code/index.js
+++ 1/code/index.js
@@ -2,6 +2,8 @@ const inputVariant = document.getElementById("variant_numb");
 const getSquareBtn = document.getElementById("get_square");
 const squareOutput = document.getElementById("square");
 
+alert("Variant 31, Oleksandr Shapovalov");
+
 getSquareBtn.addEventListener("click", () => {
   const variant = Number(inputVariant.value);
   squareOutput.textContent = Math.PI * (variant + 4) * variant ** 2;
